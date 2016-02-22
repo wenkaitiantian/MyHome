32e268b6405d65feffd630b5b40a2568e4b1c56b
yuanguangxin <274841922@qq.com>
2016-02-22 15:11:21 +0800

    add Filter

---
+++ src/com/utils/MyStrutsFilter.java
@@ -0,0 +1,25 @@
+package com.utils;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.struts2.dispatcher.ng.filter.StrutsPrepareAndExecuteFilter;
+
+public class MyStrutsFilter extends StrutsPrepareAndExecuteFilter{
+
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        String url = request.getRequestURI();
+        //System.out.println(url);
+        if (url.contains("/editor/jsp/")) {
+            System.out.println("使用自定义过滤器");
+            chain.doFilter(req, res);
+        }else{
+            super.doFilter(req, res, chain);
+        }
+    }
+}
