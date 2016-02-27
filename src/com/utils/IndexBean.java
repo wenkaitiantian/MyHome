ceb04757e9810701c5e1a14e3a320fa35fd2fd93
yuanguangxin <274841922@qq.com>
2016-02-27 21:12:22 +0800

    fix bugs

--- src/com/utils/IndexBean.java
+++ src/com/utils/IndexBean.java
@@ -27,10 +27,13 @@ public class IndexBean {
 
     public static IndexBean getIndexBean(String hql){
         IndexBean indexBean=new IndexBean();
+        int count;
         int num= Paging.getAllRowCount(hql);
-        List list=Paging.getAllList(hql);
+        if(num<=16) count=num;
+        else count = 16;
+        List list=Paging.queryByPage(hql,1,count);
         String[] path=new String[num];
-        for(int i=0;i<num;i++){
+        for(int i=0;i<count;i++){
             int n= (int)(Math.random()*4+1);
             path[i]="img/m"+n+".jpg";
         }
